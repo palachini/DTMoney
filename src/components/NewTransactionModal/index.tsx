bd6a286308b0d9f0ecbcf58c18730baed05528f2
palachini <mateuspalachini@gmail.com>
2022-07-07 17:54:03 -0400

    create form and styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container,TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +14,54 @@ export function NewTransactionModal( { isOpen , onRequestClose}: NewTransactionM
     <Modal 
           isOpen={isOpen} 
           onRequestClose={onRequestClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
         >
-          <h2>Cadastrar Transação</h2>
+          <button 
+            type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+          >
+            <img src={closeImg} alt="Fechal modal" />
+          </button>
+          
+          <Container>
+            <h2>Cadastrar Transação</h2>
+
+            <input 
+              placeholder="Título" 
+            />
+
+            <input 
+              type="number"
+              placeholder="Valor" 
+            />
+
+            <TransactionTypeContainer>
+              <button
+                type="button"  
+              >
+                <img src={incomeImg} alt="Entrada" />
+                <span>Entrada</span>
+              </button>
+              <button
+                type="button"  
+              >
+                <img src={outcomeImg} alt="Saída" />
+                <span>Entrada</span>
+              </button>
+            </TransactionTypeContainer>
+
+            <input 
+              placeholder="Categoria" 
+            />
+
+            <button 
+              type="submit"
+            >
+              Cadastrar
+            </button>
+          </Container>
       </Modal>
   ); 
 
