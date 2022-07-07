0ab70e788a03fc0a119b267261e14a706b2bd0b3
palachini <mateuspalachini@gmail.com>
2022-07-07 00:37:07 -0400

    create component modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+import Modal  from 'react-modal'; 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTranslationModalOpen, setIsNewTranslationModalOpen] = useState(false);
+
+  function handleOpenNewTranslationModal(){
+    setIsNewTranslationModalOpen(true);
+  }
+
+  function handleCloseNewTranslationModal() {
+    setIsNewTranslationModalOpen(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTranslationModal={handleOpenNewTranslationModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTranslationModalOpen}
+        onRequestClose={handleCloseNewTranslationModal}
+      />
+    
       <GlobalStyle />
     </>
   );
