f243d87bacd8862272b79e2d08d73943f4254cda
palachini <mateuspalachini@gmail.com>
2022-07-10 19:59:17 -0400

    add new transaction in transactions table

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -20,19 +20,24 @@ export function NewTransactionModal( { isOpen , onRequestClose}: NewTransactionM
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
-
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({ 
+    await createTransaction({ 
       title,
       amount,
       category,
       type,
     })
 
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setTitle('');
+
+    onRequestClose();
   }
 
   return (
