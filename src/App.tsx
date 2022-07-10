6d274761c2b62c787971c6300e9164a0adab54e2
palachini <mateuspalachini@gmail.com>
2022-07-10 19:46:40 -0400

    movie create transaction for context

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Modal  from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsContext, TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -21,7 +21,7 @@ export function App() {
   }
   
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTranslationModal={handleOpenNewTranslationModal}/>
 
       <Dashboard />
@@ -32,7 +32,7 @@ export function App() {
       />
     
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
