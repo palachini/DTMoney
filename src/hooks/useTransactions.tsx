5832dd0ffcddb632353f935a1665871e48050466
palachini <mateuspalachini@gmail.com>
2022-07-10 20:23:54 -0400

    create rook useTransaction

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
   id: number;
@@ -23,7 +23,7 @@ interface TransactionsContextData {
 }
 
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 ); 
 
@@ -53,4 +53,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         {children}
       </TransactionsContext.Provider>
     )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+  
+  return context;
 }
